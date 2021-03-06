a49c4d653655e608417d2076212ee640e9f43f32
Eliseeva Elena <243766@niuitmo.ru>
2021-03-06 10:36:44 +0300

    Добавьте файлы проекта.

---
+++ LR_1_2/Form2.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace LR_1_2
+{
+    partial class nForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // nForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.ClientSize = new System.Drawing.Size(242, 450);
+            this.Name = "nForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
